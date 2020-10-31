81bee89557c5b55dbbac448fd2fbe3aee00dc85b
super sajuk <abgaoe@gmail.com>
2020-11-01 00:41:45 +0800

    Update wbspawns.py
    fix

--- wbspawns.py
+++ wbspawns.py
@@ -6,6 +6,7 @@ import json
 import time
 from datetime import datetime  
 from datetime import timedelta
+import test
 import re
 fbchat._util.USER_AGENTS    = ["Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"]
 fbchat._state.FB_DTSG_REGEX = re.compile(r'"name":"fb_dtsg","value":"(.*?)"')
@@ -23,7 +24,6 @@ class RunMonitoring():
             "lake": 6,
             "loran": 8
         }
-
         while(True):
             messages = client.fetchThreadMessages(thread_id=thread_id, limit=10)
             messages.reverse()
@@ -32,7 +32,7 @@ class RunMonitoring():
                 if read_status == False:
                     prev_messages.append(f'{message.text}-{message.timestamp}')
                     if message.text == "/wbspawn":
-                        return_message = self.wbspawntime()
+                        return_message = test.doSomething()
                         client.send(Message(text=return_message), thread_id=thread_id, thread_type=thread_type)
                     elif message.text == "/wbscam":
                         return_message = "Ulol na Ulol"
@@ -63,24 +63,9 @@ class RunMonitoring():
                         client.send(Message(text=return_message), thread_id=thread_id, thread_type=thread_type)
             time.sleep(3)
     
-     
     
-    
-    def wbspawntime(self):
-        return_message = ""
-        spawn_loc = {
-            "soul": "harvest/elf land",
-            "8i": "karben/zahara",
-            "saint": "wildland",
-            "lake": "dawn/forest",
-            "loran": "rift/relic"   
-        }
-        with open('timestamps.json') as json_file:
-            data = json.load(json_file)
-            keys = list(data)
-            for key in keys:
-                return_message = return_message + key + f' ({spawn_loc[key]})' + " - " +data[key] +"\n"
-        return return_message
+
+        
 
 if __name__ == "__main__":
     RunMonitoring()
